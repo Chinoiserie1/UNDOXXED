37e64ac471b26b5a7a783fa5589b8ef6c22a45ec
chixx <lucottejeremie@gmail.com>
2023-06-07 13:16:52 +0200

    upgrade UNDOXXED add setter function for owner setSale

--- src/UNDOXXED.sol
+++ src/UNDOXXED.sol
@@ -32,33 +32,59 @@ contract UNDOXXED is ERC1155URIStorage, ERC1155Supply, Ownable {
   // address who mint => tokenId => mint count
   mapping(address => mapping(uint256 => uint256)) private mintCount;
 
-  event SetSale(uint256 tokenId, Sale saleInfo);
+  event SetSaleMaxPerWallet(uint256 tokenId, uint256 newMaxPerWallet);
+  event SetSaleMaxSupply(uint256 tokenId, uint256 newMaxSupply);
+  event SetSalePublicPrice(uint256 tokenId, uint256 newPublicPrice);
+  event SetSaleWhitelistPrice(uint256 tokenId, uint256 newWhitelistPrice);
   event FreezeSale(uint256 tokenId);
 
   constructor() ERC1155("UNDOXXED", "UNDX", "") {}
 
-  function setSale(uint256 _tokenId, Sale calldata _saleInfo) external onlyOwner {
-    if (saleInfo[_tokenId].freeze) revert SaleFreeze();
-    saleInfo[_tokenId] = _saleInfo;
-    emit SetSale(_tokenId, _saleInfo);
-  }
-
-  function freezeSale(uint256 _tokenId) external onlyOwner {
-    if(saleInfo[_tokenId].status == Status.notInitialised) revert SaleNotInitialised();
-    saleInfo[_tokenId].freeze = true;
-    emit FreezeSale(_tokenId);
-  }
-
   function publicMint(uint256 _tokenId, uint256 _amountMint, bytes calldata data) external payable {
     if(saleInfo[_tokenId].status != Status.started) revert SaleNotStarted();
     if(_amountMint + mintCount[msg.sender][_tokenId] > saleInfo[_tokenId].maxPerWallet) revert MaxPerWalletReach();
-    if(msg.value < saleInfo[_tokenId].publicPrice) revert IncorrectValueSend();
+    if(msg.value * _amountMint < saleInfo[_tokenId].publicPrice * _amountMint) revert IncorrectValueSend();
 
     _mint(msg.sender, _tokenId, _amountMint, data);
 
     unchecked {
       mintCount[msg.sender][_tokenId] += _amountMint;
     }
+  }
+
+  // ONLY OWNER
+  function setSaleMaxSupply(uint256 _tokenId, uint256 _maxSupply) external onlyOwner {
+    if (saleInfo[_tokenId].freeze) revert SaleFreeze();
+    saleInfo[_tokenId].maxSupply = _maxSupply;
+    emit SetSaleMaxSupply(_tokenId, _maxSupply);
+  }
+
+  function setSaleMaxPerWallet(uint256 _tokenId, uint256 _maxPerWallet) external onlyOwner {
+    if (saleInfo[_tokenId].freeze) revert SaleFreeze();
+    saleInfo[_tokenId].maxPerWallet = _maxPerWallet;
+    emit SetSaleMaxPerWallet(_tokenId, _maxPerWallet);
+  }
+
+  function setSalePublicPrice(uint256 _tokenId, uint256 _publicPrice) external onlyOwner {
+    if (saleInfo[_tokenId].freeze) revert SaleFreeze();
+    saleInfo[_tokenId].publicPrice = _publicPrice;
+    emit SetSalePublicPrice(_tokenId, _publicPrice);
+  }
+
+  function setSaleWhitelistPrice(uint256 _tokenId, uint256 _whitelistPrice) external onlyOwner {
+    if (saleInfo[_tokenId].freeze) revert SaleFreeze();
+    saleInfo[_tokenId].whitelistPrice = _whitelistPrice;
+    emit SetSaleWhitelistPrice(_tokenId, _whitelistPrice);
+  }
+
+  /**
+   * @notice freeze the sale for update maxSupply, publicPrice, whitelistPrice, maxPerWallet
+   * @param _tokenId the id of the sale
+   */
+  function freezeSale(uint256 _tokenId) external onlyOwner {
+    if(saleInfo[_tokenId].status == Status.notInitialised) revert SaleNotInitialised();
+    saleInfo[_tokenId].freeze = true;
+    emit FreezeSale(_tokenId);
   }
 
   // OVERRIDE FUNCTIONS
---
+++ test/UNDOXXED.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/UNDOXXED.sol";
+
+contract CounterTest is Test {
+  UNDOXXED public undoxxed;
+
+  uint256 internal ownerPrivateKey;
+  address internal owner;
+  uint256 internal user1PrivateKey;
+  address internal user1;
+  uint256 internal user2PrivateKey;
+  address internal user2;
+  int256 internal user3PrivateKey;
+  address internal user3;
+
+  function setUp() public {
+    ownerPrivateKey = 0xA11CE;
+    owner = vm.addr(ownerPrivateKey);
+    user1PrivateKey = 0xB0B;
+    user1 = vm.addr(user1PrivateKey);
+    user2PrivateKey = 0xFE55E;
+    user2 = vm.addr(user2PrivateKey);
+    user3PrivateKey = 0xD1C;
+    user3 = vm.addr(user2PrivateKey);
+    vm.startPrank(owner);
+
+    undoxxed = new UNDOXXED();
+  }
+
+  function 
+}
