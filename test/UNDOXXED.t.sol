fd681a9ffdb90c5375fc4e0a922747365dc8f270
chixx <lucottejeremie@gmail.com>
2023-06-22 16:25:45 +0200

    upgrade UNDOXXED add test testAllowlist2Call

--- test/UNDOXXED.t.sol
+++ test/UNDOXXED.t.sol
@@ -115,6 +115,21 @@ contract CounterTest is Test {
     require(balance == 3, "fail mint for user1");
   }
 
+  function testAllowlist2Call() public {
+    Sale memory sale = setSale(address(signer), 100, 1 ether, 0.5 ether, 3, 0, 1);
+    undoxxed.setNewSale(1, sale);
+    undoxxed.setSaleStatus(1, Status.allowlist);
+    bytes memory signature = sign(address(user1), 3, Status.allowlist);
+    vm.stopPrank();
+    vm.startPrank(user1);
+    undoxxed.allowlistMint(1, 2, 3, signature, "");
+    uint256 balance = undoxxed.balanceOf(address(user1), 1);
+    require(balance == 2, "fail mint for user1");
+    undoxxed.allowlistMint(1, 1, 3, signature, "");
+    balance = undoxxed.balanceOf(address(user1), 1);
+    require(balance == 3, "fail mint for user1");
+  }
+
   function testAllowlistFailIncorrectSignature() public {
     Sale memory sale = setSale(address(signer), 100, 1 ether, 0.5 ether, 0, 0, 1);
     undoxxed.setNewSale(1, sale);
